ab6811a39aead0bac0911cbccff41db32ebcd191
Abhishek Kumar <abhishekkumar736993@gmail.com>
2025-12-07 18:46:29 +0530

    fixed logout

--- client/src/routes/protected.route.jsx
+++ client/src/routes/protected.route.jsx
@@ -3,12 +3,19 @@ import useAuth from "../hooks/api/use-auth";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
+  const token = localStorage.getItem("accessToken");
   const { data: authData, isLoading } = useAuth();
   const user = authData?.user;
+
+  // If no token exists, redirect immediately
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
 
   if (isLoading) {
     return <DashboardSkeleton />;
   }
+  
   return user ? <Outlet /> : <Navigate to="/" replace />;
 };
 
