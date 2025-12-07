b5a0794c6a1d843e8d583996b871a9e0423415bc
Abhishek Kumar <abhishekkumar736993@gmail.com>
2025-12-07 18:36:59 +0530

    fixed side bar close button not appearing

--- client/src/components/ui/sidebar.jsx
+++ client/src/components/ui/sidebar.jsx
@@ -158,7 +158,7 @@ const Sidebar = React.forwardRef(
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground"
           >
             {children}
           </SheetContent>
